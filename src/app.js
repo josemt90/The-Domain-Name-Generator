e7a3d1509e36489d882a8b7b7f71e296939c1cce
Jose Medina Tortosa <josemedinatortosa@gmail.com>
2022-01-24 19:37:55 +0000

    Ejercicio terminado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,22 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let ext = [".com", ".es", ".net"];
+
+  let dominio = (arr1, arr2, arr3, arr4) => {
+    for (let i = 0; i < arr1.length; i++) {
+      for (let j = 0; j < arr2.length; j++) {
+        for (let h = 0; h < arr3.length; h++) {
+          for (let m = 0; m < arr4.length; m++) {
+            console.log(`${arr1[i]}${arr2[j]}${arr3[h]}${arr4[m]}`);
+          }
+        }
+      }
+    }
+  };
+
+  dominio(pronoun, adj, noun, ext);
 };
